685cf88ec76fd124e74a16a6b9118ab7c666b616
Arnel Borja <kyoushuu@yahoo.com>
2014-06-10 16:49:35 +0800

    routes/user: Show sign up page

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ if ('development' === app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+app.get('/user/new', user.create);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -17,10 +17,6 @@
  */
 
 
-/*
- * GET users listing.
- */
-
-exports.list = function(req, res) {
-  res.send('respond with a resource');
+exports.create = function(req, res) {
+  res.render('signup', {title: 'Sign Up'});
 };
