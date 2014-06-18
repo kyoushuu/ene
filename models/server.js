2a5a75fad663343ad5755d7a40f7ab4fbabfa92b
Arnel Borja <kyoushuu@yahoo.com>
2014-06-18 20:30:22 +0800

    models: Add Server model

---
+++ models/server.js
@@ -0,0 +1,73 @@
+/*
+ * ene - IRC bot for e-Sim
+ * Copyright (C) 2014  Arnel A. Borja <kyoushuu@yahoo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+var mongoose = require('mongoose');
+
+
+var serverSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  shortname: {
+    type: String, required: true, unique: true, lowercase: true,
+    validate: {
+      validator: /^[a-z]$/i,
+      msg: 'Short name should be a single letter',
+    },
+  },
+});
+
+serverSchema.path('name').validate(function(value, respond) {
+  Server.find({
+    _id: {$ne: this._id},
+    name: value,
+  }, function(error, servers) {
+    if (error) {
+      console.log(error);
+      return respond(false);
+    }
+
+    if (servers.length) {
+      respond(false);
+    } else {
+      respond(true);
+    }
+  });
+}, 'Server name already exists');
+
+serverSchema.path('shortname').validate(function(value, respond) {
+  Server.find({
+    _id: {$ne: this._id},
+    shortname: value,
+  }, function(error, servers) {
+    if (error) {
+      console.log(error);
+      return respond(false);
+    }
+
+    if (servers.length) {
+      respond(false);
+    } else {
+      respond(true);
+    }
+  });
+}, 'Server short name already exists');
+
+/* jshint -W003 */
+var Server = mongoose.model('Server', serverSchema);
+/* jshint +W003 */
+module.exports = Server;
