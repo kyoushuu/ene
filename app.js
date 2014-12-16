fd3c72c957317f61a5c93fcdc0f6bbf838686ae1
Arnel Borja <kyoushuu@yahoo.com>
2014-12-16 19:30:14 +0800

    app: Add a favicon

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
 
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -77,6 +78,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
