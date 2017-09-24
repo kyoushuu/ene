1e0d7339c580469b7a3a24dcbfefc4266d8dff98
Arnel Borja <kyoushuu@yahoo.com>
2017-09-24 13:20:02 +0800

    app: Pass error directly to error handler

--- app.js
+++ app.js
@@ -120,12 +120,9 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.render('error', {error});
   });
 }
 
